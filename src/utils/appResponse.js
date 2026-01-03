4557dd9b8c5d45e6862091dc041cbf67ecf06c48
SHAH JI <abhinavshah133@gmail.com>
2026-01-04 01:12:20 +0530

    fix the folder problem

--- src/utils/appResponse.js
+++
@@ -1,9 +0,0 @@
-class AppResponse {
-  constructor(statusCode, message = "Success", data) {
-    this.statusCode = statusCode;
-    this.message = message;
-    this.data = data;
-    this.status = statusCode < 400;
-  }
-}
-export { AppResponse };
